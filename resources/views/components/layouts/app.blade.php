44b0b8bafe5446d8758595fd3666d1704b3a2450
informaticalivreoficial <renatomontanari@ymail.com>
2026-05-07 15:51:33 -0300

    update posts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,8 @@
 
     <link rel="icon" href="{{ asset('theme/images/chave.png')}}" type="image/x-icon">
     
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     {{-- FontAwesome --}}
     <link rel="stylesheet" href="{{ asset('theme/plugins/fontawesome-free/css/all.min.css') }}">
     {{-- Bootstrap 4 --}}
